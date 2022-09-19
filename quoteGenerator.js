2684740e4c3015d4c4aa1a419a50aca432881e1b
TeacherMiguel <miguelespinosaflores@gmail.com>
2022-09-19 08:09:12 -0500

    first animations

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,32 +1,32 @@
 let btn = document.getElementById("newQuote");
-let quotes = document.querySelector('.quote');
+let quotes = document.getElementById('quote');
 let persons = document.querySelector('.person');
 let juego = document.querySelector('.videogame');
 
 
 
 const quotations = [{
-    quote: "We all make choices but in the end our choices make us",
+    quote: '"We all make choices but in the end our choices make us"',
     person: "Andrew Ryan",
     game: "Bioshock"
 },
 {
-    quote: "Sir, Finishing this fight",
+    quote: '"Sir, Finishing this fight"',
     person: "Master chief",
     game: "Halo2"
 },
 {
-    quote: "Get over here!",
+    quote: '"Get over here!"',
     person: "Scorpion",
     game: "Mortal Kombat"
 },
 {
-    quote: "Life is cruel. Of this I have no doubt. One can only hope that one leaves behind a lasting legacy. But so often, the legacies we leave behind... are not the ones we intended",
+    quote: '"Life is cruel. Of this I have no doubt. One can only hope that one leaves behind a lasting legacy. But so often, the legacies we leave behind... are not the ones we intended"',
     person: "Queen Myrrah",
     game: "Gears of war 2"
 },
 {
-    quote: "Do a barrel roll!",
+    quote: '"Do a barrel roll!"',
     person: "Peppy Hare",
     game: "Star fox 64"
 },
@@ -40,21 +40,43 @@ persons.innerHTML = currentGame.person;
 juego.innerHTML = currentGame.game;
 
 
+
 function checkQuotes() {
-    let redo = quotations.length -1
-    
+
+    let redo = quotations.length - 1
+
     if (contador == redo) {
-        contador=0;
+        contador = 0;
+
         quotes.innerHTML = quotations[contador].quote
         persons.innerHTML = quotations[contador].person
         juego.innerHTML = quotations[contador].game
+
+
     }
     else {
         contador++
+        
         quotes.innerHTML = quotations[contador].quote
         persons.innerHTML = quotations[contador].person
-        juego.innerHTML = quotations[contador].game
-
+        juego.innerHTML = quotations[contador].game;
+        quotes.classList.remove('animate');
+        persons.classList.remove('animate')
+        juego.classList.remove('animate')
+        quotes.offsetWidth;
+        quotes.classList.add('animate');
+        persons.classList.add('animate');
+        juego.classList.add('animate');
+        
     }
+
 }
 
+// const explmButton = document.getElementById('ExampleButton')
+
+// explmButton.onclick = () => {
+//     explmButton.classList.add('Animation')
+// }
+// explmButton.onanimationend = () => {
+//     explmButton.classList.remove('Animation')
+// }
